cc32d5fefcd122679bb0960d0a575052008cc1fe
Rodrigueza02 <mariaj.rodrigueza@campusucc.edu.co>
2026-04-20 12:30:32 -0500

    Update typography to Poppins and Crimson Pro for modern elegant design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,20 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
+import { Geist_Mono, Poppins, Crimson_Pro } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  weight: ['300', '400', '500', '600', '700'],
+})
+
+const crimsonPro = Crimson_Pro({
+  subsets: ['latin'],
+  variable: '--font-heading',
+  weight: ['400', '500', '600', '700'],
+})
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,7 +30,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
+      className={cn("antialiased", fontMono.variable, poppins.variable, crimsonPro.variable)}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
